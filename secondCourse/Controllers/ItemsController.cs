5b64fa152564a712a67b8f43090884ae193ff129
Bereket <Bereket@DESKTOP-VQSU47E>
2024-12-11 16:25:56 -0800

    Add project files.

---
+++ secondCourse/Controllers/ItemsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using secondCourse.Models;
+
+namespace secondCourse.Controllers
+{
+    public class ItemsController : Controller
+    {
+        public IActionResult Overview()
+        {
+            var item = new Item() { Name = "keyboard" };
+            return View(item);
+        }
+    }
+}
